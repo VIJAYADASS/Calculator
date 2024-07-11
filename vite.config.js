de0c3c20d78c9e98f825e21a606df2b6150ff74d
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2024-07-11 18:55:48 +0530

    Change the Config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   rollupOptions: {
-  //     external: ["mathjs"],
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          mathjs: ["mathjs"], // Add more chunks as needed
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
+  },
 });
